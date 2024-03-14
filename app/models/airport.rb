4b41113a2fa97fdb92942a22d6c554b1eddb2757
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-03-13 17:36:13 -0700

    Add initial associations to flight and airport models

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,4 @@
 class Airport < ApplicationRecord
+  has_many :departing_flights, class_name: "Flight", dependent: :destroy
+  has_many :arriving_flights, class_name: "Flight", dependent: :destroy
 end
