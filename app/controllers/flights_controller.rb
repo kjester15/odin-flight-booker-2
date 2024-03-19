cdbf15436b7d0b954293a7dadad63a0291e49988
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-03-19 16:17:47 -0700

    Removed unnecessary @dates from Flights controller #index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,7 +3,6 @@ class FlightsController < ApplicationController
     @flights = Flight.all
     @departure_airports = Flight.all.map { |flight| [flight.departure_airport.code, flight.departure_airport_id] }.uniq.sort
     @arrival_airports = Flight.all.map { |flight| [flight.arrival_airport.code, flight.arrival_airport_id] }.uniq.sort
-    @dates = Flight.all.map { |flight| [flight.departure_date_formatted, flight.id] }.sort
     @passengers = [[1, 1], [2, 2], [3, 3], [4, 4]]
 
     @results = Flight.where(departure_airport_id: params[:departure_airport_id]).where(arrival_airport_id: params[:arrival_airport_id]).where(departure_date: params[:departure_date])
