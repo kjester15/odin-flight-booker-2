885b2ebfda3a68e42a7d19a83c6cc858efbef6b1
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-03-20 18:17:32 -0700

    Create #new and prelim #create in Bookings controller and add bookings routes to config

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,21 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = EventUser.new(booking_params)
+
+    if @booking.save
+      # redirect_to events_path
+    else
+      # redirect_to events_path, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, :passengers)
+  end
 end
