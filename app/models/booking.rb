fda01a3c8cb27a727f4c59bd94106c269177ab84
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-03-20 18:12:48 -0700

    Add associations to models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,4 @@
 class Booking < ApplicationRecord
+  belongs_to :flights
+  belongs_to :passengers
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+  has_many :bookings
 
   def departure_date_formatted
     departure_date.strftime("%m/%d/%Y")
