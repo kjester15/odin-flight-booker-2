08bd6973fbf481ba2e4f8fc1a2eac05e82828407
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-04-26 12:59:03 -0700

    Create prelim #booking_email action in passenger mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,8 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def booking_email
+    @passenger = params[:passenger]
+    mail(to: @user.email, subject: 'Your flight has been booked!')
+  end
 end
